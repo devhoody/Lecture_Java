20817a5e9916001bf8787a429cb8a3773dba56ec
devhoody <rethnoly@gmail.com>
2023-04-18 17:33:03 +0900

    OOPJavaPrj

---
+++ Newlecture_OOPJavaPrj1/src/Program.java
@@ -0,0 +1,14 @@
+import part3.ex4.UI코드분리하기.Exam;
+
+public class Program {
+
+	public static void main(String[] args) {
+	NewlecExam exam = new NewlecExam(1,1,1,5);
+	
+	
+	
+	System.out.println(exam.total());
+	System.out.println(exam.avg());
+	}
+	
+}
