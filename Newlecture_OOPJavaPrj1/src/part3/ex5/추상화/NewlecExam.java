c35b5999bd769f178c15f91e3a30fe1985a0001e
devhoody <rethnoly@gmail.com>
2023-04-19 20:25:40 +0900

    추상

---
+++ Newlecture_OOPJavaPrj1/src/part3/ex5/추상화/NewlecExam.java
@@ -0,0 +1,36 @@
+package part3.ex5.√ﬂªÛ»≠;
+
+public class NewlecExam extends Exam {
+	
+	int com;
+	
+	public NewlecExam() {
+		this(0,0,0,0);
+	}
+	
+	public NewlecExam(int kor, int eng, int math, int com) {
+		super(kor,eng,math);
+		this.com = com;
+	}
+	
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	}
+
+	@Override
+	public int total() {
+		int total = onTotal() + getCom();
+		return total;
+	}
+
+	@Override
+	public float avg() {
+
+		return total()/ 4.0f;
+	}
+	
+}
---
+++ Newlecture_OOPJavaPrj1/src/part3/ex5/추상화/Program.java
@@ -0,0 +1,16 @@
+package part3.ex5.√ﬂªÛ»≠;
+
+public class Program {
+
+	public static void main(String[] args) {
+//		Exam exam = new NewlecExam();
+		
+		ExamConsole console = new NewlecExamConsole();
+		
+		console.Input();
+		console.Print();
+		
+		
+	}
+
+}
