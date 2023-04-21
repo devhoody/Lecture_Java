bae3b025ad79c30c0aef4ac360f4a422f4834f08
devhoody <rethnoly@gmail.com>
2023-04-21 22:13:34 +0900

    객체지향 - 인터페이

--- Newlecture_OOPJavaPrj1/src/part3/ex6/인터페이스/Program.java
+++ Newlecture_OOPJavaPrj1/src/part3/ex6/인터페이스/Program.java
@@ -1,12 +1,21 @@
 package part3.ex6.인터페이스;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Program {
 
-	public static void main(String[] args) {
-		A a = new A();
-		B b = new B(); //배터리
+	public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+		FileInputStream fis = new FileInputStream("src/part3/ex6/인터페이스/setting.txt");
+		Scanner scan = new Scanner(fis);
+		String className = scan.nextLine();
 		
-		a.setX(b);
+		Class clazz = Class.forName(className);
+		A a = new A();
+		X x = (X) clazz.newInstance(); //배터리
+		
+		a.setX(x);
 		a.print();
 	}
 
