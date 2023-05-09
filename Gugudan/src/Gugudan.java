2e4caec3b310ba84b485cab84eb9a66546baddb4
devhoody <rethnoly@gmail.com>
2023-05-09 16:55:03 +0900

    while, for을 이용한 구구단

--- Gugudan/src/Gugudan.java
+++ Gugudan/src/Gugudan.java
@@ -81,5 +81,17 @@ public class Gugudan {
 		System.out.println(number +"단: " + number * 8);
 		System.out.println(number +"단: " + number * 9);
 		
+		int i =1;
+		System.out.println("while을 이용한 "+number+"단은 다음과 같습니다.");
+		while(i<10) {
+			System.out.println(number +"단: " + number * i);
+			i++;
+		}
+		
+		System.out.println("For을 이용한 "+number+"단은 다음과 같습니다.");
+		for(i=1; i<10; i++) {
+			System.out.println(number +"단: " + number * i);
+		}
+		
 	}
 }
