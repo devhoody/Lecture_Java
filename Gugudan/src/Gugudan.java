0169b2039d910862aac9f75a4fa4af8a39fc8714
devhoody <rethnoly@gmail.com>
2023-05-16 21:03:31 +0900

    Gugudan class이

--- Gugudan/src/Gugudan.java
+++ Gugudan/src/Gugudan.java
@@ -1,22 +1,17 @@
-import java.util.Scanner;
-
 public class Gugudan {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		// 반복문 - while
-		int i = 1;
-		System.out.println("while을 이용한 " + number + "단은 다음과 같습니다.");
-		while (i < 10) {
-			System.out.println(number + "단: " + number * i);
-			i++;
-		}
+	
+	public static int[] calculate(int times) {
+		int[] result = new int[9];
 
-		// 반복문 - for
-		System.out.println("For을 이용한 " + number + "단은 다음과 같습니다.");
-		for (i = 1; i < 10; i++) {
-			System.out.println(number + "단: " + number * i);
+		for (int i = 0; i < result.length; i++) {
+			result[i] = times * (i + 1);
 		}
+		return result;
+	}
 
+	public static void print(int[] result) {
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
 	}
 }
